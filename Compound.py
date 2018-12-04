0a8fb4f857f11d7e2d9bf522b9bbe599140a2e52
Wineclaw <45408153+Wineclaw@users.noreply.github.com>
2018-12-04 12:35:06 -0500

    Add files via upload

---
+++ Compound.py
@@ -0,0 +1,19 @@
+# A simple compounding interest script that prints the final compounded amount
+# after asking the user for inputs.
+#
+# By: Wineclaw, December 4, 2018
+
+# Asks user for information
+
+P = int(input("What is the principal amount?"))
+r = float(input("What is the interest rate (in decimals)?"))
+n = int(input("How many times per year is the interest compounded?"))
+t = int(input("How many years will the money be compounded for?"))
+
+# Final amount equation, probably wrong
+
+final_amount = P * (1 + (r/n)) ** (n*t)
+
+# prints final amount after t years
+
+print("The final amount after", t, "years is $", final_amount)
